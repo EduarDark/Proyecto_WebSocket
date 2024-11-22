d6645967403467136c452b5d81178f2f84f35db1
EduarDark <edu.tejada2003@gmail.com>
2024-11-22 04:35:34 -0400

    cliente y servidor añadidos mas la interfaz

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
+const http = require('http');
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-    console.log('Nuevo jugador conectado');
-    ws.on('message', (message) => {
-        console.log('Mensaje recibido:', message);
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    });
+    ws.send('¡Bienvenido al servidor!');
 });
+
+server.listen(8080, () => console.log('Servidor en http://localhost:8080'));
